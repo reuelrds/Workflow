e62d92cb355ea09d07ec520354508c9ec7ad0470
Reuel D'silva <reuelrds@gmail.com>
2019-02-14 12:58:38 +0530

    removed authentication to access client and admin for developing them

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,8 +7,10 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 const routes: Routes = [
   {path: '', component: LandingPageComponent, pathMatch: 'full'},
   {path: 'auth', loadChildren: './auth/auth.module#AuthModule'},
-  {path: 'admin-panel', loadChildren: './panels/admin-panel/admin-panel.module#AdminPanelModule', canActivate: [AuthGuard]},
-  {path: 'client-panel', loadChildren: './panels/client-panel/client-panel.module#ClientPanelModule', canActivate: [AuthGuard]}
+  // {path: 'admin-panel', loadChildren: './panels/admin-panel/admin-panel.module#AdminPanelModule', canActivate: [AuthGuard]},
+  // {path: 'client-panel', loadChildren: './panels/client-panel/client-panel.module#ClientPanelModule', canActivate: [AuthGuard]}
+  {path: 'admin-panel', loadChildren: './panels/admin-panel/admin-panel.module#AdminPanelModule'},
+  {path: 'client-panel', loadChildren: './panels/client-panel/client-panel.module#ClientPanelModule'}
 ];
 
 @NgModule({
