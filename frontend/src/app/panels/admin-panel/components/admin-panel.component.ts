1ecb8c1432b557c2341049a002f388f1ccbbc4c2
Reuel D'silva <reuelrds@gmail.com>
2019-03-02 21:34:06 +0530

    Updated Admin Panel template to include tabbed interface

--- frontend/src/app/panels/admin-panel/components/admin-panel.component.ts
+++ frontend/src/app/panels/admin-panel/components/admin-panel.component.ts
@@ -3,6 +3,9 @@ import { AdminService } from '../../../core/services/admin.service';
 import { Observable } from 'rxjs';
 import { Admin } from '../../../shared/models/admin';
 import { AuthService } from '../../../core/services/auth.service';
+import { AppsManagementComponent } from './apps-management/apps-management.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { UserManagementComponent } from './user-management/user-management.component';
 
 @Component({
   selector: 'app-admin-panel',
@@ -13,18 +16,21 @@ export class AdminPanelComponent implements OnInit {
 
   admin: Admin = {name: '' , email: ''};
 
+  tabs = [{label: 'User Management', component: UserManagementComponent},
+          {label: 'App Management', component: AppsManagementComponent},
+          {label: 'Account Settings', component: AccountSettingsComponent}];
 
   constructor(private adminService: AdminService, private authService: AuthService) { }
 
   ngOnInit() {
-    this.adminService.getAdminData().subscribe(res => {
-      console.log(res);
-      this.admin = res;
-    });
+    // this.adminService.getAdminData().subscribe(res => {
+      // console.log(res);
+      // this.admin = res;
+    // });
   }
 
   onLogout() {
-    this.authService.logout();
+    // this.authService.logout();
   }
 
 }
