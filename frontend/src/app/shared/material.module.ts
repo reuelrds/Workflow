6bc1efb9fe7cc9606c15c64d1a40a225bc6a2bde
Reuel D'silva <reuelrds@gmail.com>
2019-03-02 21:32:02 +0530

    Updated to include required Matreial Modules

--- frontend/src/app/shared/material.module.ts
+++ frontend/src/app/shared/material.module.ts
@@ -1,12 +1,40 @@
 import { NgModule } from '@angular/core';
-import { MatProgressSpinnerModule, MatDialogModule, MatButtonModule, MatToolbarModule } from '@angular/material';
+import { MatProgressSpinnerModule,
+   MatDialogModule,
+   MatButtonModule,
+   MatToolbarModule,
+   MatTabsModule,
+   MatCardModule,
+   MatSelectModule,
+   MatFormFieldModule,
+   MatInputModule,
+   MatSlideToggleModule,
+   MatDatepickerModule,
+   MatNativeDateModule,
+   MatSidenavModule,
+   MatDividerModule,
+   MatRippleModule,
+   MatExpansionModule,
+   MatIconModule} from '@angular/material';
 
 @NgModule({
   exports: [
     MatProgressSpinnerModule,
     MatButtonModule,
     MatDialogModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTabsModule,
+    MatCardModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatDatepickerModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDividerModule,
+    MatRippleModule,
+    MatExpansionModule
   ]
 })
 export class MaterialModule { }
