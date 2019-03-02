779927129cde993a97e9ad0c40c3bf8cd0ef1545
Reuel D'silva <reuelrds@gmail.com>
2019-03-02 21:33:05 +0530

    declare three tab components for Admin Pannel

--- frontend/src/app/panels/admin-panel/admin-panel.module.ts
+++ frontend/src/app/panels/admin-panel/admin-panel.module.ts
@@ -5,14 +5,20 @@ import { AdminPanelRoutingModule } from './admin-panel-routing.module';
 import { AdminPanelComponent } from './components/admin-panel.component';
 import { SharedModule } from '../../shared/shared.module';
 import { MaterialModule } from '../../shared/material.module';
+import { UserManagementComponent } from './components/user-management/user-management.component';
+import { AccountSettingsComponent } from './components/account-settings/account-settings.component';
+import { AppsManagementComponent } from './components/apps-management/apps-management.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     AdminPanelRoutingModule,
     SharedModule,
+    ReactiveFormsModule,
     MaterialModule
   ],
-  declarations: [AdminPanelComponent]
+  declarations: [AdminPanelComponent, UserManagementComponent, AccountSettingsComponent, AppsManagementComponent],
+  entryComponents: [AppsManagementComponent, UserManagementComponent, AccountSettingsComponent]
 })
 export class AdminPanelModule { }
