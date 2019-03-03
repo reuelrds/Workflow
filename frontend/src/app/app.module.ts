f29ca7274efeb7a79985940f31adcf802529928c
Reuel D'silva <reuelrds@gmail.com>
2019-03-04 02:07:08 +0530

    Changed Auth Module from Lazy to Eagerly loaded

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { LandingPageModule } from './landing-page/landing-page.module';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SharedModule } from './shared/shared.module';
     LandingPageModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    AuthModule
   ],
   bootstrap: [AppComponent]
 })
--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,24 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from './../shared/shared.module';
+import { MaterialModule } from '../shared/material.module';
 
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { CompanySignupComponent } from './signup/company-signup/company-signup.component';
 import { EmployeeSignupComponent } from './signup/employee-signup/employee-signup.component';
 
-import { AuthRoutingModule } from './auth-routing.module';
-import { MaterialModule } from '../shared/material.module';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
-    AuthRoutingModule,
-    FormsModule,
     ReactiveFormsModule,
     MaterialModule
   ],
@@ -26,7 +22,7 @@ import { MaterialModule } from '../shared/material.module';
     LoginComponent,
     SignupComponent,
     CompanySignupComponent,
-    EmployeeSignupComponent,
+    EmployeeSignupComponent
   ]
 })
 export class AuthModule { }
