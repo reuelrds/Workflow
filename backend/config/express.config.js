b6292082e9217d2d11cecc4a278a439c76fb220f
Reuel D'silva <reuelrds@gmail.com>
2019-03-06 00:40:15 +0530

    added cors package

--- backend/config/express.config.js
+++ backend/config/express.config.js
@@ -2,11 +2,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
 const config = require('./var.config');
 
 // Initializing Express
 const app = express();
+
+// Implementing CORS 
+app.use(cors());
 
 // BodyParser parses incomming data stream into objects that can be used
 app.use(bodyParser.json());
@@ -15,18 +19,5 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use("/images", express.static(path.join(config.imageFolder)));
 
-// Implementing CORS 
-app.use((req,res,next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-      'Access-Control-Allow-Headers',
-       "Origin, X-Request-Width, Content-Type, Accept, Authorization");
-  res.setHeader(
-      'Access-Control-Allow-Methods', 
-      "GET, POST, PATCH, PUT, DELETE, OPTIONS");
-  res.setHeader(
-    'Access-Control-Max-Age', '86400');
-  next();
-});
 
 module.exports = app;
