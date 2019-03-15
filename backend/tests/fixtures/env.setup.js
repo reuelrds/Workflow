1d8dfb86e59d77cc4a995c95b21aff2744a51d34
Reuel D'silva <reuelrds@gmail.com>
2019-03-16 03:30:55 +0530

    Add script to cleanup uploads folder after every test

--- backend/tests/fixtures/env.setup.js
+++ backend/tests/fixtures/env.setup.js
@@ -1,2 +1,14 @@
 const yargs = require('yargs');
+const fs = require('fs-extra');
+
+// Set Environment vairable to use test config
 yargs.parse(['--env', 'test']);
+
+
+// Cleaning up uploads folder after every test
+try {
+  fs.emptyDirSync('./backend/tests/files/uploads');
+  fs.ensureDirSync('./backend/tests/files/uploads/avatars');
+} catch(err) {
+  console.log(err);
+}
