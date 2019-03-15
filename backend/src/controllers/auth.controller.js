d9ba8fb7056d41e0b71e4eb9b72bb7ade0c66bfa
Reuel D'silva <reuelrds@gmail.com>
2019-03-15 19:02:54 +0530

    Refactored to include user signup with no token/companyId

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -17,68 +17,74 @@ exports.createUser = async (req, res, next) => {
     // Hashing password
     const hashedPasswd = await bcrypt.hash(req.body.password, config.bcrypt.saltLength);
 
-    // fetching admin for the user
-    const admin = await Admin.findOne({id: req.body.token});
+    // baseUrl for image
+    const url = req.protocol + "://" +req.get("host");
     
-    if(!admin) {
-      throw new Error("The Admin Account for your Organization doesn't exist. Please create one Before registering any users");
+    // Creating user object to be saved into the database
+    let imgpath;
+    if(!req.file) {
+      imgpath = "null";
     } else {
-      console.log(admin);
-      if (admin.id === req.body.token) { // this check is redundant
+      imgpath = req.file.filename;
+    }
 
-        // baseUrl for image
-        const url = req.protocol + "://" +req.get("host");
-        
-        // Creating user object to be saved into the database
-        let imgpath;
-        if(!req.file) {
-          imgpath = "null";
-        } else {
-          imgpath = req.file.filename;
-        }
-        const user = new User({
-          profileImagePath: url+"/images/" + imgpath,
-          firstName: req.body.firstName,
-          lastName: req.body.lastName,
-          email: req.body.email,
-          password: hashedPasswd,
-          companyId: admin.id
-        });
+    let userData = {
+      profileImagePath: url+"/images/" + imgpath,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: hashedPasswd,
+      id: uniqid()
+    };
 
-        // Saving admin credentials to the database
-        const result = await user.save();
+    if(req.body.token) {
 
-        const jwtToken = jwt.sign({
-          email: req.body.email,
-        password: req.body.password
-        }, config.jwtsecret, {
-          expiresIn: "1h"
-        });
-    
-        // Saving successfull
-        res.status(201).json({
-          message: "User Created!",
-          jwtToken,
-          usertype: 'User',
-          expiresIn: 3600,
-          userId: admin.id
-        });     
+      // fetching admin for the user
+      const admin = await Admin.findOne({id: req.body.token});
+      
+      if(!admin) {
+        throw new Error("The Admin Account for your Organization doesn't exist. Please create one Before registering any users");
       } else {
-        throw new Error("Invalid Token");
+        console.log(admin);
+        userData = {
+          ...userData,
+          companyId: admin.id
+        };
       }
     }
+
+    const newUser = new User(userData);
+
+
+    // Saving admin credentials to the database
+    const result = await newUser.save();
+
+    const jwtToken = jwt.sign({
+      email: req.body.email,
+      password: req.body.password
+    }, config.jwtsecret, {
+      expiresIn: "1h"
+    });
+
+    // Saving successfull
+    res.status(201).json({
+      message: "User Created!",
+      jwtToken,
+      usertype: 'User',
+      expiresIn: 3600,
+      userId: result.id
+    });     
   } catch(err) {
     console.log(err);
-
     res.status(500).json({
-      message: `Creating User Failed! ${err.message}`
+      message: 'Creating User Failed!'
     });
   }
 };
 
 // Creating admin
 exports.createAdmin = async (req, res, next) => {
-  console.log(req.body);
+  // console.log(req.body);
   try {
 
     // Hashing Password
@@ -95,7 +101,7 @@ exports.createAdmin = async (req, res, next) => {
     // Saving admin credentials to the database
     const result = await admin.save();
 
-    console.log("vervebeb", result);
+    // console.log("vervebeb", result);
     const jwtToken = jwt.sign({
       email: req.body.email,
       password: req.body.password
