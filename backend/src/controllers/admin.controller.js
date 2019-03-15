88bc9df3da60140c3a3bc3ebcf6ba216b5b5ffa8
Reuel D'silva <reuelrds@gmail.com>
2019-03-16 03:58:51 +0530

    Updated User & Admin endpoints to return more appopriate status code

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -5,14 +5,14 @@ exports.getAdminData = async (req, res, next) => {
 
     const AdminData = await Admin.findOne({id: req.params.id});
     console.log(AdminData);
-    res.status(201).json({
+    res.status(200).json({
       name: AdminData.name,
       email: AdminData.email
     });
     
   } catch (error) {
-    res.status(500).json({
-      message: "Admin Data not Found"
+    res.status(404).json({
+      message: "Admin Data Not Found"
     })
   }
 }
