9461581ee8896666907be00eec4b5b34b96220e9
Reuel D'silva <reuelrds@gmail.com>
2019-03-20 12:07:12 +0530

    set up route to add a new user

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -1,6 +1,11 @@
-const Admin = require("./../models/admin.model");
+const uniqid = require('uniqid');
+const bcrypt = require('bcryptjs');
 
-exports.getAdminData = async (req, res, next) => {
+const config = require('./../config/var.config');
+const Admin = require("./../models/admin.model");
+const User = require("./../models/user.model");
+
+const getAdminData = async (req, res, next) => {
   try {
 
     const AdminData = await Admin.findOne({id: req.params.id});
@@ -15,4 +20,49 @@ exports.getAdminData = async (req, res, next) => {
       message: "Admin Data Not Found"
     })
   }
+}
+
+const addUser = async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const admin = await Admin.findOne({id: req.userData.userId});
+    
+    if(!admin) {
+      throw new Error('Invalid Request');
+    }
+
+    const id = uniqid()
+    const hashedPassword = await bcrypt.hash(id, config.bcrypt.saltLength);
+
+    const newUser = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: hashedPassword,
+      id:id
+    };
+
+    const result = await User(newUser).save()
+    console.log(result);
+
+    res.status(200).json({
+      message: "Added User",
+      user: {
+        firstName: result.firstName,
+        lastName: result.lastName,
+        email: result.email,
+        id: result.id
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      message: "Not Found"
+    })
+  }
+}
+
+module.exports = {
+  getAdminData,
+  addUser
 }
--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const CheckAuth = require('./../middleware/check-auth');
 const AdminController = require('./../controllers/admin.controller');
 
 router.get('/:id', AdminController.getAdminData);
+
+router.post('/add-new-user', CheckAuth, AdminController.addUser);
 
 module.exports = router;
