babc8ba4d0905b70346de4baeb440816573d4dfe
Reuel D'silva <reuelrds@gmail.com>
2019-03-30 13:43:18 +0530

    Added tests for get all-users endpoint

--- backend/tests/user/user.test.js
+++ backend/tests/user/user.test.js
@@ -6,12 +6,22 @@ const app = require('./../../src/app');
 const db = require('./../../src/config/mongoose.config');
 
 const User = require('./../../src/models/user.model');
+const Admin = require('./../../src/models/admin.model');
 
 
 describe('Testing User Endpoint', () => {
+  let token = null;
   beforeEach(async () => {
     await db.connect();
     await dbSetup.setupDatabase();
+    const adminLogin = await request(app)
+      .post('/api/auth/login')
+      .set('Authorization', 'Bearer ' + token)
+      .send({
+      email: dbSetup.adminOne.email,
+      password: '123456'
+    });
+    token = adminLogin.body.jwtToken;
   });
 
   afterEach(async () => {
@@ -19,7 +29,10 @@ describe('Testing User Endpoint', () => {
   });
 
   test('it should fetch user details', async () => {
-    const res = await request(app).get(`/api/user/${dbSetup.userOne.id}`).expect(200);
+    const res = await request(app)
+      .get(`/api/user/${dbSetup.userOne.id}`)
+      .set('Authorization', 'Bearer ' + token)
+      .expect(200);
 
     expect(res.body).toMatchObject({
       firstName: dbSetup.userOne.firstName,
@@ -30,7 +43,10 @@ describe('Testing User Endpoint', () => {
 
   test('it should throw an error if an invalid id is passed', async () => {
     try {
-      await request(app).get('/api/user/error')
+      await request(app)
+        .get(`/api/user/error`)
+        .set('Authorization', 'Bearer ' + token)
+      
     } catch (error) {
       expect(error.status).toBe(404);
       expect(error.response.body.message).toContain('User Data Not Found');
@@ -75,5 +91,57 @@ describe('Testing User Endpoint', () => {
         }
       });
     })
+
+    test('it should throw an error if no admin is found', async () => {
+      try {
+        await Admin.deleteOne({id: dbSetup.adminOne.id});
+        await request(app)
+          .post('/api/user/add-new-user')
+          .set('Authorization', `Bearer ${token}`)
+          .send({
+            firstName: 'new',
+            lastName: 'test',
+            email: 'new@email.com'
+          });
+      } catch (error) {
+        // console.log(error);
+        expect(error.status).toBe(404);
+        expect(error.response.body.message).toContain("Admin Not Found. Invalid Request");
+      }
+    });
+  });
+
+  describe('testing all-users endpoint', () => {
+
+    test('it should fetch all users associated with the auth token', async () => {
+
+      const res = await request(app)
+        .get('/api/user/all-users')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200);
+
+      const {profileImagePath, companyId, password, ...userDetails} = dbSetup.userTwo;
+      const expectedUsers = [
+        userDetails
+      ];
+  
+      expect(res.body.users).toEqual(expectedUsers);
+    });
+
+    test('it should throw an error if no admin is found', async () => {
+      try {
+        await Admin.deleteOne({id: dbSetup.adminOne.id});
+        await request(app)
+          .get('/api/user/all-users')
+          .set('Authorization', `Bearer ${token}`);
+      } catch (error) {
+        expect(error.status).toBe(404);
+        expect(error.response.body.message).toContain("Admin Not Found. Invalid Request");
+      }
+
+      
+    })
+
+
   });
 });
