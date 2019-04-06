d4954d644afb54bbe578a028b128d588c48c63a9
Reuel D'silva <reuelrds@gmail.com>
2019-04-06 19:02:44 +0530

    Added: Department Dialog

--- frontend/src/app/panels/admin-panel/components/user-management/dialogs/department-dialog/department-dialog.component.ts
+++ frontend/src/app/panels/admin-panel/components/user-management/dialogs/department-dialog/department-dialog.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { Store, select } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+
+import { AddDepartmentDialogComponent } from './dialog/add-department-dialog/add-department-dialog.component';
+
+import { Department } from 'src/app/shared/models/department';
+
+import * as fromAdminPanel from '../../../../store/admin-panel.reducers';
+import * as DepartmentActions from './../../../../store/department/department.action';
+import * as fromDepartmentSelector from './../../../../store/department/department.selectors';
+import * as fromUserSelector from './../../../../store/users/user.selectors';
+import * as UserActions from '../../../../store/users/user.actions';
 
 @Component({
   selector: 'app-department-dialog',
@@ -7,9 +20,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartmentDialogComponent implements OnInit {
 
-  constructor() { }
+  addDepartmentDialog: MatDialogRef<AddDepartmentDialogComponent>;
+  dataSet: Observable<Department[]>;
+  cols: string[] = ['select', 'departmentName', 'departmentHead', 'totalStaff'];
+
+  constructor(
+    private dialog: MatDialog,
+    private store: Store<fromAdminPanel.State>) { }
 
   ngOnInit() {
+    this.store.dispatch(new DepartmentActions.TryGetDepartments());
+    this.store.dispatch(new UserActions.TryGetUsers());
+    // const departmentState = this.store.pipe(select(fromDepartmentSelector.getAllDepartments));
+    const departmentState = this.store.pipe(select(fromDepartmentSelector.dep));
+    // const userState = this.store.pipe(select(fromUserSelector.getAllUsers));
+    departmentState.subscribe(departments => this.dataSet = of(departments));
+  }
+
+  openAddDepartmentDialog() {
+    this.addDepartmentDialog = this.dialog.open(AddDepartmentDialogComponent, {
+      width: 'max-content',
+      height: 'max-content',
+      minWidth: '35vw',
+      minHeight: '20vh',
+      autoFocus: false
+    });
+
+    this.addDepartmentDialog.afterClosed().subscribe(result => {
+      if (result) {
+        this.store.dispatch({
+          type: DepartmentActions.ActionTypes.TryAddDepartment,
+          payload: result.value
+        });
+      }
+    });
   }
 
 }
