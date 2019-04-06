807188b826d7537309d140448d3912c387180ea3
Reuel D'silva <reuelrds@gmail.com>
2019-04-06 19:03:47 +0530

    Added: A Template to add a new department

---
+++ frontend/src/app/panels/admin-panel/components/user-management/dialogs/department-dialog/dialog/add-department-dialog/add-department-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-department-dialog',
+  templateUrl: './add-department-dialog.component.html',
+  styleUrls: ['./add-department-dialog.component.scss']
+})
+export class AddDepartmentDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
