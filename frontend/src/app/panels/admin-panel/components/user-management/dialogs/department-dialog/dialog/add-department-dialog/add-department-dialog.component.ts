75078c724c32cab134e1af34d4ec79b03c505eee
Reuel D'silva <reuelrds@gmail.com>
2019-04-07 13:20:03 +0530

    Refactor: Removed unused imports and variables

--- frontend/src/app/panels/admin-panel/components/user-management/dialogs/department-dialog/dialog/add-department-dialog/add-department-dialog.component.ts
+++ frontend/src/app/panels/admin-panel/components/user-management/dialogs/department-dialog/dialog/add-department-dialog/add-department-dialog.component.ts
@@ -1,13 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material';
-import { Store } from '@ngrx/store';
 
-import * as fromAdminPanel from '../../../../../../store/admin-panel.reducers';
-import * as DepartmentActions from '../../../../../../store/department/department.action';
-import { Observable } from 'rxjs';
-import { User } from 'src/app/shared/models/user';
-import { identifier } from '@babel/types';
 
 @Component({
   selector: 'app-add-department-dialog',
@@ -20,7 +14,6 @@ export class AddDepartmentDialogComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private store: Store<fromAdminPanel.AdminPanelState>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
 
