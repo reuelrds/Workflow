4cab8d5e24ee2b2eaeb80c1311bdda8f20d162ed
Reuel D'silva <reuelrds@gmail.com>
2019-04-07 13:17:10 +0530

    Updated: Add department form to work for adding as well as updating departments

--- frontend/src/app/panels/admin-panel/components/user-management/dialogs/department-dialog/dialog/add-department-dialog/add-department-dialog.component.ts
+++ frontend/src/app/panels/admin-panel/components/user-management/dialogs/department-dialog/dialog/add-department-dialog/add-department-dialog.component.ts
@@ -7,6 +7,7 @@ import * as fromAdminPanel from '../../../../../../store/admin-panel.reducers';
 import * as DepartmentActions from '../../../../../../store/department/department.action';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/shared/models/user';
+import { identifier } from '@babel/types';
 
 @Component({
   selector: 'app-add-department-dialog',
@@ -24,9 +25,19 @@ export class AddDepartmentDialogComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    let name = '';
+    let headId = '';
+    let departmentId = '';
+
+    if (this.data.department) {
+      name = this.data.department.departmentName;
+      headId = this.data.department.departmentHead;
+      departmentId = this.data.department.id;
+    }
     this.addDepartmentForm = this.formBuilder.group({
-      departmentName: '',
-      departmentHead: '',
+      departmentName: name,
+      departmentHead: headId,
+      id: departmentId
     });
   }
 
