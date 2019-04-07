f5ebbe717781530ef94d2114ae795011e81bc9d8
Reuel D'silva <reuelrds@gmail.com>
2019-04-07 13:14:59 +0530

    Added: A call to update a department's name or head

--- frontend/src/app/core/services/admin.service.ts
+++ frontend/src/app/core/services/admin.service.ts
@@ -12,7 +12,6 @@ import { Department } from 'src/app/shared/models/department';
   providedIn: 'root'
 })
 export class AdminService {
-
   /**
    * @param {string} BACKEND_URL URL ponting to the backend server.
    */
@@ -30,7 +29,6 @@ export class AdminService {
 
   time;
 
-
   /**
    * Injecting required Services
    *
@@ -38,8 +36,10 @@ export class AdminService {
    * @param httpClient Used for HTTP requests.
    *
    */
-  constructor(private authService: AuthService, private httpClient: HttpClient) {
-  }
+  constructor(
+    private authService: AuthService,
+    private httpClient: HttpClient
+  ) {}
 
   getAdminId() {
     // this.authService.getUserIdListener().subscribe(result => {
@@ -53,47 +53,79 @@ export class AdminService {
       this.getAdminId();
     }
 
-
-    return this.httpClient.get<{name: string, email: string}>(`${this.BACKEND_URL}/api/admin/${this.adminId}`).pipe(tap(result => {
-
-      console.log('evrgr');
-      console.log(result);
-      this.adminData = result;
-      console.log(this.adminData.name);
-    }));
+    return this.httpClient
+      .get<{ name: string; email: string }>(
+        `${this.BACKEND_URL}/api/admin/${this.adminId}`
+      )
+      .pipe(
+        tap(result => {
+          console.log('evrgr');
+          console.log(result);
+          this.adminData = result;
+          console.log(this.adminData.name);
+        })
+      );
   }
 
   addNewUser(userData) {
     console.log('service: ', userData);
-    return this.httpClient.post<{message: string, user: User}>(`${this.BACKEND_URL}/api/user/add-new-user`, userData);
+    return this.httpClient.post<{ message: string; user: User }>(
+      `${this.BACKEND_URL}/api/user/add-new-user`,
+      userData
+    );
   }
 
   getUsers() {
-    return this.httpClient.get<{users: User[]}>(`${this.BACKEND_URL}/api/user/all-users`);
+    return this.httpClient.get<{ users: User[] }>(
+      `${this.BACKEND_URL}/api/user/all-users`
+    );
   }
 
   updateUserManager(userId, newManagerId) {
     console.log(userId);
     let managerId;
     if (!newManagerId) {
-      managerId = {managerId: undefined};
+      managerId = { managerId: undefined };
     } else {
-      managerId = {managerId: newManagerId};
+      managerId = { managerId: newManagerId };
     }
     // return this.httpClient.patch<{message: string, user: User}>(`${this.BACKEND_URL}/api/user/update-manager/${userId}`, managerId);
-    return this.httpClient.patch<{message: string, user: User, manager: User}>(
-      `${this.BACKEND_URL}/api/user/update-manager/${userId}`, managerId
-    );
+    return this.httpClient.patch<{
+      message: string;
+      user: User;
+      manager: User;
+    }>(`${this.BACKEND_URL}/api/user/update-manager/${userId}`, managerId);
   }
 
   getDepartments() {
-    return this.httpClient.get<{departments: Department[]}>(`${this.BACKEND_URL}/api/department/all-departments`);
+    return this.httpClient.get<{ departments: Department[] }>(
+      `${this.BACKEND_URL}/api/department/all-departments`
+    );
   }
 
   addNewDepartment(newDepartmentData) {
-    return this.httpClient.post<{message: string, department: Department}>(
-        `${this.BACKEND_URL}/api/department/new-department`,
-        newDepartmentData
+    return this.httpClient.post<{ message: string; department: Department }>(
+      `${this.BACKEND_URL}/api/department/new-department`,
+      newDepartmentData
     );
+  }
+
+  updateDepartment(updatedDetails) {
+    console.log(updatedDetails);
+    if (updatedDetails.updateField === 'name') {
+      return this.httpClient.patch<{ message: string; department: Department }>(
+        `${this.BACKEND_URL}/api/department/${
+          updatedDetails.department.id
+        }/updateDepartmentName`,
+        { newDepartmentName: updatedDetails.department.departmentName }
+      );
+    } else if (updatedDetails.updateField === 'head') {
+      return this.httpClient.patch<{ message: string; department: Department }>(
+        `${this.BACKEND_URL}/api/department/${
+          updatedDetails.department.id
+        }/updateDepartmentHead`,
+        { newDepartmentHead: updatedDetails.department.departmentHead }
+      );
+    }
   }
 }
